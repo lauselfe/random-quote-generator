d3e17bcbc9b796f881e3c2e342a4e14021cf3728
Laura <lauselfe@gmail.com>
2020-09-13 22:52:14 +0200

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,112 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+
+
+class App extends React.Component{
+ 
+
+  constructor(){
+    super()
+    this.state = {
+      apiResult : null, 
+      author: '',
+      quote : '',
+      isLoaded : false,
+      bgColor : '#c2c2c2'
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+   
+  }
+
+  componentDidMount(){
+    fetch ('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json', {
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then((responseData) => {
+      this.setState({
+        apiResult: responseData.quotes, 
+        isLoaded: true,
+        author: responseData.quotes[0].author,
+        quote: responseData.quotes[0].quote,
+        quotesArrayLength: responseData.quotes.length, 
+      })
+    })
+    .catch(error => this.setState({ error }))
+  }
+
+  generateQuote = () => {
+    const chosenQuote = []
+    const quotes = this.state.apiResult
+    let randomNumber = Math.floor((Math.random() * this.state.apiResult.length) + 1)
+
+    quotes.map((element, index) => {
+      if( index === randomNumber) {
+        chosenQuote.push(element)
+      }
+    })
+
+    return this.setState({
+      quote: chosenQuote[0].quote, 
+      author:chosenQuote[0].author,
+    })
+
+  }
+
+  changeColor = () => {
+      const color = ['#ff6f59', '#254441', '#43aa8b', '#b2b09b', '#ef3054']
+      let randomColor = Math.floor((Math.random() * color.length - 1) + 1);
+      let valueOfRandomColor = color[randomColor] 
+      console.log(randomColor.valueOf())
+     return this.setState({
+       bgColor: valueOfRandomColor
+        
+      })
+  }
+
+
+
+  handleChange(){
+    
+    this.generateQuote()
+    this.changeColor()
+  }
+
+  render(){
+    
+    return (    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <style>
+        {
+          `   
+          :root {
+            --main-bg-color: ${this.state.bgColor};
+            --main-text-color: ${this.state.bgColor};
+          }
+          `
+        }
+     
+      </style>
+      <h1>Random Quote Box Generator</h1>
+      <div id="quote-box" className="quote-box">
+       
+    <p id="text" className="text-style" >{this.state.quote}</p>
+    <p id="author" className="author-style" >{this.state.author}</p>
+        <button id="new-quote" onClick={this.handleChange}>Random quote</button>
+        <br />
+        <a href="#" id="tweet-quote">Tweet this!</a>
+      </div>
+
+      <footer>Developed by <a href="http://www.github.com/lauselfe">@lauselfe</a></footer>
     </div>
-  );
+    
+    )
+  }
+  
 }
 
 export default App;
---
+++ src/quotesData.js
@@ -0,0 +1,33 @@
+const quotesData = [{
+        id: 1,
+        quote: "Going to the mountains is like going home.",
+        author: "John Muir",
+    },
+    {
+        id: 2,
+        quote: "To forget how to dig the earth and to tend the soil is to forget ourselves",
+        author: "Mahatma Gandhi"
+    },
+    {
+        id: 3, 
+        quote: "Nature is pleased with simplicity. And nature is no dummy.",
+        author: "Isaac Newton"
+    },
+    {
+        id: 4, 
+        quote: "Birds have always had the ability to bring me out of a dark space and provide relief in bad times.",
+        author: "Jason Ward" 
+    },
+    {
+        id: 5, 
+        quote: "Adopt the pace of nature. Her secret is patience.",
+        author: "Ralph Waldo Emerson"
+    },
+    {
+        id: 6, 
+        quote: "Choose only one master—nature.",
+        author: "Rembrandt"
+    }
+]
+
+export default quotesData
